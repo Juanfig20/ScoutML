403cbd02dc6cb4f230701402d94c90038e67d134
Juanfig20 <juan.figuera@correo.unimet.edu.ve>
2025-09-08 19:28:44 -0400

    Modificada url

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.predictions.urls')),
+    path('api/predictions/', include('backend.predictions.urls')),
     path('api/users/', include('backend.users.urls')),
     path('api/billing/', include('backend.billing.urls')),
 ]
